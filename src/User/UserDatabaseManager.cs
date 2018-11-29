b3a3b810e9d06cd267d1fd266e8e43460ce8a6c8
Roiit2912 <rohit.rathi2912@gmail.com>
2018-11-29 12:21:10 +0530

    migrations added

--- src/User/UserDatabaseManager.cs
+++ src/User/UserDatabaseManager.cs
@@ -7,64 +7,58 @@ using System.Security.Cryptography;
 
 namespace JwtTokenSpace
 {
-    public class UserDatabaseManager :IUserRepository
+    public class UserDatabaseManager : IUserRepository
     {
 
-        UserDatabase db_obj =null;
+        UserDatabase db_obj = null;
 
         public UserDatabaseManager(UserDatabase db_obj1)
         {
-            this.db_obj=db_obj1;
+            this.db_obj = db_obj1;
         }
+
+        // This fuction get the user from the Database
 
         public User GetUser(string email)
         {
-            using(db_obj)
-            {
-                 try
-              {
-                  return db_obj.users.First(user => user.Email.Equals(email));
-              } catch
-              {
-                  return null;
-              }
 
+            try
+            {
+                return db_obj.users.First(user => user.Email.Equals(email));
             }
-             
+            catch
+            {
+                return null;
+            }
+
+
+
         }
 
-            public string Hash(string password)
+        // This function Hash the password
+
+        public string Hash(string password)
+        {
+            using (var sha256 = SHA256.Create())
             {
-                using(var sha256 = SHA256.Create())  
-                {  
-                    // Converting Password to Hash.  
-                    var hashedBytes = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));  
-                    // Get the hashed string.  
-                    return System.BitConverter.ToString(hashedBytes);  
-                }  
+                // Converting Password to Hash.  
+                var hashedBytes = sha256.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                // Get the hashed string.  
+                return System.BitConverter.ToString(hashedBytes);
             }
+        }
 
-            
 
-            public void Register(User obj)
-            {  
-                using(db_obj)
-                {
-                    db_obj.users.Add(obj);
-                    db_obj.SaveChanges();
+        // This function register the user in the DataBase
 
-                }
-               
-                
+        public void Register(User obj)
+        {
 
-            }
+            db_obj.users.Add(obj);
+            db_obj.SaveChanges();
 
-    ~UserDatabaseManager()
-    {
+        }
 
-    db_obj.Dispose();
-
-    }
 
     }
 }
